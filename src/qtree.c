fb8e11762f03b89d667b850e1f49d8e9ae82e617
Andrew Shi <164560703+andrewshi2027@users.noreply.github.com>
2024-10-31 03:00:00 +0000

    10:59PM

--- src/qtree.c
+++ src/qtree.c
@@ -10,13 +10,6 @@ QTNode *create_quadtree(Image *image, double max_rmse) {
         return NULL;
     }
 
-    //Base Case: Area is 1x1
-    if (width == 1 && height == 1) {
-        node->intensity = image->pixels[0][0];
-        node->child1 = node->child2 = node->child3 = node->child4 = NULL;
-        return node;
-    }
-
     //Calculate Average Intensity of the current region
     unsigned char sum = 0;
     for (int i = 0; i < height; i++) {
@@ -36,6 +29,10 @@ QTNode *create_quadtree(Image *image, double max_rmse) {
         return node;
     }
 
+    //Split into 4 quadrants
+    int half_width = width / 2;
+    int half_height = height / 2;
+
 
 
 
@@ -45,23 +42,27 @@ QTNode *create_quadtree(Image *image, double max_rmse) {
 }
 
 QTNode *get_child1(QTNode *node) {
-    (void)node;
-    return NULL;
+    return node->child1;
+    // (void)node;
+    // return NULL;
 }
 
 QTNode *get_child2(QTNode *node) {
-    (void)node;
-    return NULL;
+    return node->child2;
+    // (void)node;
+    // return NULL;
 }
 
 QTNode *get_child3(QTNode *node) {
-    (void)node;
-    return NULL;
+    return node->child3;
+    // (void)node;
+    // return NULL;
 }
 
 QTNode *get_child4(QTNode *node) {
-    (void)node;
-    return NULL;
+    return node->child4;
+    // (void)node;
+    // return NULL;
 }
 
 unsigned char get_node_intensity(QTNode *node) {
@@ -71,7 +72,11 @@ unsigned char get_node_intensity(QTNode *node) {
 }
 
 void delete_quadtree(QTNode *root) {
-    (void)root;
+    if (root) {
+        delete_quadtree(root->child1);
+
+    }
+    //(void)root;
 }
 
 void save_qtree_as_ppm(QTNode *root, char *filename) {
